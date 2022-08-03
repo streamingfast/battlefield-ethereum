d6b01bb79d1925c961f6c0cfa92a3183e2d0b4bf
Matthieu Vachon <matt@streamingfast.io>
2022-08-03 14:55:59 -0400

    Adjusted a bit how `authrpcFlags` are defined

--- bin/library.sh
+++ bin/library.sh
@@ -36,8 +36,8 @@ export syncer_oe_cmd="$oe_bin --base-path=${syncer_oe_data_dir}"
 
 export bootstrap_data_dir="$RUN_DIR/data/bootstrap"
 
-is_old_geth() {
-  if geth version 2>/dev/null | grep -Eq "1.9.10-(fh[0-9]+|dm)"; then
+is_authrpc_supported() {
+  if geth version 2>/dev/null | grep -Eq "1\.10\.20-(fh[0-9]+|dm)"; then
     echo "true"
   else
     echo "false"
