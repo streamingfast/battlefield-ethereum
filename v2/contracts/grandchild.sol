c4342870ba2794e36d40f5d958bae708be164b3e
Matthieu Vachon <matt@streamingfast.io>
2024-11-27 09:32:33 -0500

    Added contracts

---
+++ v2/contracts/grandchild.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.6.6 <0.7.0;
+
+contract GrandChild {
+    uint256 callerGas;
+    uint256 callerGasLeft;
+    uint256 gasLeft;
+
+    constructor(address payable to, bool fail) public payable {
+        to.transfer(msg.value);
+        if (fail) {
+            to.transfer(msg.value + 9000000000000000);
+        }
+    }
+
+    function recordGasLeft(uint256 _callerGas, uint256 _callerGasLeft) public {
+        callerGas = _callerGas;
+        callerGasLeft = _callerGasLeft;
+        gasLeft = gasleft();
+
+        emit eventGasLeft(gasleft(), msg.sender);
+    }
+
+    function emptyCallForLowestGas() public pure {}
+
+    event eventGasLeft(uint256 gas, address sender);
+}
--- v2/hardhat.config.ts
+++ v2/hardhat.config.ts
@@ -1,8 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
-};
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.12",
+      },
+      {
+        version: "0.8.27",
+      },
+    ],
+  },
+}
 
-export default config;
+export default config
