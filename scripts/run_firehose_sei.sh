a0a187b99af61fce2009062c2ed275e574ebfd3d
Matthieu Vachon <matt@streamingfast.io>
2025-04-01 16:08:18 -0400

    Fixed Sei chain's data
    Seems that we might need to fix that as it seems it might be needed to re-do the initial state of each hard fork. At least it was required this time :(

--- scripts/run_firehose_sei.sh
+++ scripts/run_firehose_sei.sh
@@ -28,18 +28,26 @@ main() {
   cp -R "$ROOT/sei_dev/chain_data" "$run_dir/sei"
   sd 'occ-enabled *=.*' "occ-enabled = ${occ_enabled}" "$run_dir/sei/config/app.toml"
 
-  launch_funder &
+  launch_funder "$run_dir/sei" &
 
   echo "Running Sei node with Firehose tracer activated via 'fireeth'"
   run_fireeth 1 "$seid" "start --home \"$run_dir/sei\" --trace --chain-id sei-chain"
 }
 
 launch_funder() {
+    home_dir="$1"
+
     # A first sleep to let the node start
     sleep 2
     ensure_parent_runs "$PARENT_PID"
 
+    ls "$home_dir" || {
+        echo "Home dir '$home_dir' does not exist"
+        exit 1
+    }
+
     echo "Funding $address_to_fund"
+    echo "- Home dir: $1"
 
     tries=0
     while true; do
@@ -48,12 +56,13 @@ launch_funder() {
         tries=$((tries + 1))
         if [ $tries -gt 10 ]; then
             echo "Unable to fund address after 10 tries"
+            killall "$seid"
             kill $PARENT_PID
             exit 1
         fi
 
         set +e
-        "$seid" tx evm send --from=admin "$address_to_fund" 10000000000000000000000000 -b block -y
+        "$seid" --home "$home_dir" tx evm send --from=admin "$address_to_fund" 10000000000000000000000000 -b block -y
         if [[ $? -ne 0 ]]; then
             echo "Failed to send the funding transaction, retrying in 1s"
         else
