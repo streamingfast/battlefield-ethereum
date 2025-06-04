44a38a1c46e3d9d504be3d7dc68bdcec47ceb9f2
David Zhou <david.zhou.0110@gmail.com>
2025-06-04 14:38:36 -0400

    TEST: Dynamic testing to test polygon only when on the correct chain

--- test/polygon.test.ts
+++ test/polygon.test.ts
@@ -5,52 +5,55 @@ import { isSameAddress, systemAddress, knownExistingAddress } from "./lib/addres
 import { Call } from "../pb/sf/ethereum/type/v2/type_pb"
 import { owner } from "./global"
 import { oneWei } from "./lib/money"
+import { isNetwork } from "./lib/network"
 
 // Polygon system contract addresses from validatorContract in data/bor/genesis.json
 const VALIDATOR_CONTRACT_ADDRESS = "0x0000000000000000000000000000000000001000"
 
-describe("Polygon", function () {
-  it("Polygon System Transaction", async function () {
-    // Find a block that contains a validator set update system transaction
-    const spanEndBlock = await findSpanEndBlock()
-    if (!spanEndBlock) {
-      return
-    }
+if (isNetwork("polygon-dev")) {
+  describe("Polygon", function () {
+    it("Polygon System Transaction", async function () {
+      // Find a block that contains a validator set update system transaction
+      const spanEndBlock = await findSpanEndBlock()
+      if (!spanEndBlock) {
+        return
+      }
 
-    const firehoseBlock = await fetchFirehoseBlock(spanEndBlock)
-    const systemTx = firehoseBlock.systemCalls.find(isValidatorSetUpdateCall)
+      const firehoseBlock = await fetchFirehoseBlock(spanEndBlock)
+      const systemTx = firehoseBlock.systemCalls.find(isValidatorSetUpdateCall)
 
-    expect(systemTx).to.not.be.undefined
-    expect(isSameAddress(systemTx!.caller, systemAddress)).to.be.true
-    expect(isSameAddress(systemTx!.address, VALIDATOR_CONTRACT_ADDRESS)).to.be.true
-    expect(systemTx!.callType).to.be.equal(1)
-    expect(systemTx!.gasConsumed).to.be.greaterThan(0)
-    expect(systemTx!.storageChanges.length).to.be.greaterThan(0)
-    expect(firehoseBlock.transactionTraces.some(tx =>
-      tx.calls.some(call => call === systemTx)
-    )).to.be.true
-  })
+      expect(systemTx).to.not.be.undefined
+      expect(isSameAddress(systemTx!.caller, systemAddress)).to.be.true
+      expect(isSameAddress(systemTx!.address, VALIDATOR_CONTRACT_ADDRESS)).to.be.true
+      expect(systemTx!.callType).to.be.equal(1)
+      expect(systemTx!.gasConsumed).to.be.greaterThan(0)
+      expect(systemTx!.storageChanges.length).to.be.greaterThan(0)
+      expect(firehoseBlock.transactionTraces.some(tx =>
+        tx.calls.some(call => call === systemTx)
+      )).to.be.true
+    })
 
-  it("Polygon ETH Transfer Logs", async function () {
-    const result = await sendEth(owner, knownExistingAddress, oneWei)
-    const { block, trace } = await fetchFirehoseTransactionAndBlock(result)
-    expect(block.number).to.be.equal(result.blockNumber)
+    it("Polygon ETH Transfer Logs", async function () {
+      const result = await sendEth(owner, knownExistingAddress, oneWei)
+      const { block, trace } = await fetchFirehoseTransactionAndBlock(result)
+      expect(block.number).to.be.equal(result.blockNumber)
 
-    // Verify top-level logs
-    expect(trace.receipt).to.not.be.undefined
-    expect(trace.receipt!.logs).to.be.an('array')
-    expect(trace.receipt!.logs).to.have.length.above(0)
+      // Verify top-level logs
+      expect(trace.receipt).to.not.be.undefined
+      expect(trace.receipt!.logs).to.be.an('array')
+      expect(trace.receipt!.logs).to.have.length.above(0)
 
-    // Verify internal logs
-    expect(trace.calls).to.be.an('array')
-    expect(trace.calls.length).to.be.greaterThan(0)
+      // Verify internal logs
+      expect(trace.calls).to.be.an('array')
+      expect(trace.calls.length).to.be.greaterThan(0)
 
-    trace.calls.forEach((call, index) => {
-      expect(call.logs).to.be.an('array')
-      expect(call.logs).to.have.length.above(0)
+      trace.calls.forEach((call, index) => {
+        expect(call.logs).to.be.an('array')
+        expect(call.logs).to.have.length.above(0)
+      })
     })
   })
-})
+}
 
 async function findSpanEndBlock(): Promise<number | null> {
   const latestBlock = await mustGetRpcBlock("latest")
