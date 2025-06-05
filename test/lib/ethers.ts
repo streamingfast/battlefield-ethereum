e3f352c3e86af8e6bd3dd582ab8368fc7b6f88ac
David Zhou <david.zhou.0110@gmail.com>
2025-06-05 10:22:35 -0400

    TEST: Improved log testing

--- test/lib/ethers.ts
+++ test/lib/ethers.ts
@@ -1,5 +1,7 @@
-import { TransactionReceipt, TransactionResponse } from "ethers"
+import { accessListify, hexlify, Signature, TransactionReceipt, TransactionResponse } from "ethers"
 import hre from "hardhat"
+import { Block, CallType, TransactionTrace } from "../../pb/sf/ethereum/type/v2/type_pb"
+import { toBigInt } from "./numbers"
 
 /**
  * A TransactionReceipt with additional fields to store the nonce and the response
@@ -50,5 +52,109 @@ export async function waitForTransaction(
   out.nonce = BigInt(response.nonce)
   out.response = response
 
+  return out
+}
+
+export async function getReceiptForTransactionTrace(
+  block: Block,
+  trxTrace: TransactionTrace
+): Promise<TransactionReceiptResult> {
+  let effectiveGasPriceCalc: bigint
+  if (trxTrace.type === 2) {
+    // EIP-1559
+    const baseFeePerGas = block.header?.baseFeePerGas ? toBigInt(block.header.baseFeePerGas) : 0n
+    const maxPriorityFeePerGas = trxTrace.maxPriorityFeePerGas ? toBigInt(trxTrace.maxPriorityFeePerGas) : 0n
+    const maxFeePerGas = trxTrace.maxFeePerGas ? toBigInt(trxTrace.maxFeePerGas) : 0n
+
+    const sumFee = baseFeePerGas + maxPriorityFeePerGas
+    effectiveGasPriceCalc = sumFee < maxFeePerGas ? sumFee : maxFeePerGas
+    // Ensure effective gas price is not less than base fee if maxFeePerGas is high enough
+    if (effectiveGasPriceCalc < baseFeePerGas && maxFeePerGas >= baseFeePerGas) {
+      effectiveGasPriceCalc = baseFeePerGas
+    } else if (maxFeePerGas < baseFeePerGas) {
+      // If user's max_fee_per_gas is less than current base_fee
+      effectiveGasPriceCalc = maxFeePerGas
+    }
+  } else {
+    // Legacy (type 0) or EIP-2930 (type 1)
+    effectiveGasPriceCalc = toBigInt(trxTrace.gasPrice)
+  }
+
+  const receipt = new TransactionReceipt(
+    {
+      to: trxTrace.calls[0].callType === CallType.CREATE ? null : hexlify(trxTrace.to),
+      from: hexlify(trxTrace.from),
+      contractAddress: trxTrace.calls[0].callType === CallType.CREATE ? hexlify(trxTrace.to) : null,
+      index: trxTrace.index,
+      root:
+        trxTrace.receipt?.stateRoot && trxTrace.receipt.stateRoot.length > 0
+          ? hexlify(trxTrace.receipt.stateRoot)
+          : null,
+      gasUsed: toBigInt(trxTrace.gasUsed),
+      logsBloom:
+        trxTrace.receipt?.logsBloom && trxTrace.receipt.logsBloom.length > 0
+          ? hexlify(trxTrace.receipt.logsBloom)
+          : `0x${"00".repeat(256)}`,
+      blockHash: hexlify(block.hash),
+      hash: hexlify(trxTrace.hash),
+      logs:
+        trxTrace.receipt?.logs.map((log) => ({
+          transactionHash: hexlify(trxTrace.hash),
+          blockHash: hexlify(block.hash),
+          blockNumber: Number(block.number),
+          removed: false, // Assuming logs are not removed for traced transactions
+          address: hexlify(log.address),
+          data: hexlify(log.data),
+          topics: log.topics.map((t) => hexlify(t)),
+          index: Number(log.index), // log.index is log's position in block
+          transactionIndex: trxTrace.index,
+        })) ?? [],
+      blockNumber: Number(block.number),
+      cumulativeGasUsed: trxTrace.receipt?.cumulativeGasUsed ? toBigInt(trxTrace.receipt.cumulativeGasUsed) : 0n,
+      gasPrice: trxTrace.type === 2 ? null : effectiveGasPriceCalc,
+      effectiveGasPrice: effectiveGasPriceCalc,
+      blobGasUsed: block.header?.blobGasUsed,
+      blobGasPrice: trxTrace.blobGas,
+      type: trxTrace.type,
+      status: trxTrace.status === 1 ? 1 : trxTrace.status === 2 ? 0 : null, // SUCCEEDED: 1, FAILED: 0, REVERTED (also 0 in practice for status), UNKNOWN: null
+    },
+    hre.ethers.provider
+  )
+
+  const out = new TransactionReceiptResult(receipt, receipt.provider)
+  out.nonce = trxTrace.nonce
+  out.response = new TransactionResponse(
+    {
+      hash: hexlify(trxTrace.hash),
+      blockNumber: Number(block.number),
+      blockHash: hexlify(block.hash),
+      index: trxTrace.index,
+      type: trxTrace.type,
+      to: trxTrace.to && trxTrace.to.length > 0 ? hexlify(trxTrace.to) : null,
+      from: hexlify(trxTrace.from),
+      nonce: Number(trxTrace.nonce),
+      gasLimit: toBigInt(trxTrace.gasLimit),
+      gasPrice: toBigInt(trxTrace.gasPrice),
+      maxPriorityFeePerGas: toBigInt(trxTrace.maxPriorityFeePerGas),
+      maxFeePerGas: toBigInt(trxTrace.maxFeePerGas),
+      data: hexlify(trxTrace.input),
+      value: toBigInt(trxTrace.value),
+      chainId: BigInt(0),
+      signature: Signature.from({
+        r: hexlify(trxTrace.r),
+        s: hexlify(trxTrace.s),
+        v: trxTrace.v[0], // Assuming v is Uint8Array of length 1
+      }),
+      accessList: accessListify(
+        trxTrace.accessList.map((item) => ({
+          address: hexlify(item.address),
+          storageKeys: item.storageKeys.map((k) => hexlify(k)),
+        }))
+      ),
+      maxFeePerBlobGas: toBigInt(trxTrace.blobGas),
+      blobVersionedHashes: trxTrace.blobHashes.map((h) => hexlify(h)),
+    },
+    hre.ethers.provider
+  )
   return out
 }
--- test/polygon.test.ts
+++ test/polygon.test.ts
@@ -1,10 +1,10 @@
 import { expect } from "chai"
 import { fetchFirehoseBlock, fetchFirehoseTransactionAndBlock } from "./lib/firehose"
-import { mustGetRpcBlock, sendEth } from "./lib/ethereum"
+import { contractCall, mustGetRpcBlock, sendEth } from "./lib/ethereum"
 import { isSameAddress, systemAddress, knownExistingAddress } from "./lib/addresses"
 import { Call } from "../pb/sf/ethereum/type/v2/type_pb"
 import { owner } from "./global"
-import { oneWei } from "./lib/money"
+import { oneWei, wei } from "./lib/money"
 import { isNetwork } from "./lib/network"
 
 // Polygon system contract addresses from validatorContract in data/bor/genesis.json
@@ -14,23 +14,29 @@ if (isNetwork("polygon-dev")) {
   describe("Polygon", function () {
     it("Polygon System Transaction", async function () {
       // Find a block that contains a validator set update system transaction
-      const spanEndBlock = await findSpanEndBlock()
-      if (!spanEndBlock) {
-        return
-      }
+      // const spanEndBlock = await findSpanEndBlock()
+      // if (!spanEndBlock) {
+      //   return
+      // }
 
-      const firehoseBlock = await fetchFirehoseBlock(spanEndBlock)
-      const systemTx = firehoseBlock.systemCalls.find(isValidatorSetUpdateCall)
+      const firehoseBlock = await fetchFirehoseBlock(64)
+      const tx = firehoseBlock.transactionTraces[0]
+      await expect(
+        [tx, firehoseBlock]
+      ).to.trxTraceEqualSnapshot("system_tx/validator_update.expected.json", {
+      })
 
-      expect(systemTx).to.not.be.undefined
-      expect(isSameAddress(systemTx!.caller, systemAddress)).to.be.true
-      expect(isSameAddress(systemTx!.address, VALIDATOR_CONTRACT_ADDRESS)).to.be.true
-      expect(systemTx!.callType).to.be.equal(1)
-      expect(systemTx!.gasConsumed).to.be.greaterThan(0)
-      expect(systemTx!.storageChanges.length).to.be.greaterThan(0)
-      expect(firehoseBlock.transactionTraces.some(tx =>
-        tx.calls.some(call => call === systemTx)
-      )).to.be.true
+    //   const systemTx = firehoseBlock.systemCalls.find(isValidatorSetUpdateCall)
+    //
+    //   expect(systemTx).to.not.be.undefined
+    //   expect(isSameAddress(systemTx!.caller, systemAddress)).to.be.true
+    //   expect(isSameAddress(systemTx!.address, VALIDATOR_CONTRACT_ADDRESS)).to.be.true
+    //   expect(systemTx!.callType).to.be.equal(1)
+    //   expect(systemTx!.gasConsumed).to.be.greaterThan(0)
+    //   expect(systemTx!.storageChanges.length).to.be.greaterThan(0)
+    //   expect(firehoseBlock.transactionTraces.some(tx =>
+    //     tx.calls.some(call => call === systemTx)
+    //   )).to.be.true
     })
 
     it("Polygon ETH Transfer Logs", async function () {
@@ -41,16 +47,83 @@ if (isNetwork("polygon-dev")) {
       // Verify top-level logs
       expect(trace.receipt).to.not.be.undefined
       expect(trace.receipt!.logs).to.be.an('array')
-      expect(trace.receipt!.logs).to.have.length.above(0)
+      expect(trace.receipt!.logs).to.have.lengthOf(2)
 
-      // Verify internal logs
+      // Validate transaction receipt structure
+      expect(trace.receipt!.$typeName).to.equal("sf.ethereum.type.v2.TransactionReceipt")
+      expect(trace.receipt!.stateRoot).to.be.instanceOf(Uint8Array)
+      expect(trace.receipt!.cumulativeGasUsed).to.be.a('bigint')
+      expect(trace.receipt!.cumulativeGasUsed).to.equal(63600n)
+      expect(trace.receipt!.logsBloom).to.be.instanceOf(Buffer)
+      expect(trace.receipt!.logsBloom).to.have.lengthOf(256)
+
+      const receiptLog1 = trace.receipt!.logs[0]
+      expect(receiptLog1.$typeName).to.equal("sf.ethereum.type.v2.Log")
+      expect(receiptLog1.address).to.be.instanceOf(Buffer).with.lengthOf(20)
+      expect(receiptLog1.topics).to.be.an('array').with.lengthOf(4)
+      expect(receiptLog1.topics[0]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(receiptLog1.topics[1]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(receiptLog1.topics[2]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(receiptLog1.topics[3]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(receiptLog1.data).to.be.instanceOf(Buffer).with.lengthOf(160)
+      expect(receiptLog1.index).to.equal(0)
+      expect(receiptLog1.blockIndex).to.equal(4)
+      expect(receiptLog1.ordinal).to.be.a('bigint')
+
+      const receiptLog2 = trace.receipt!.logs[1]
+      expect(receiptLog2.$typeName).to.equal("sf.ethereum.type.v2.Log")
+      expect(receiptLog2.address).to.be.instanceOf(Buffer).with.lengthOf(20)
+      expect(receiptLog2.topics).to.be.an('array').with.lengthOf(4)
+      expect(receiptLog2.topics[0]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(receiptLog2.topics[1]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(receiptLog2.topics[2]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(receiptLog2.topics[3]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(receiptLog2.data).to.be.instanceOf(Buffer).with.lengthOf(160)
+      expect(receiptLog2.index).to.equal(1)
+      expect(receiptLog2.blockIndex).to.equal(5)
+      expect(receiptLog2.ordinal).to.be.a('bigint')
+
+      expect(receiptLog2.ordinal).to.be.greaterThan(receiptLog1.ordinal)
+
+      // Verify internal calls
       expect(trace.calls).to.be.an('array')
-      expect(trace.calls.length).to.be.greaterThan(0)
+      expect(trace.calls).to.have.lengthOf(1)
 
-      trace.calls.forEach((call, index) => {
-        expect(call.logs).to.be.an('array')
-        expect(call.logs).to.have.length.above(0)
-      })
+      const singleCall = trace.calls[0]
+      expect(singleCall).to.exist
+      expect(singleCall.index).to.equal(1)
+      expect(singleCall.parentIndex).to.equal(0)
+      expect(singleCall.depth).to.equal(0)
+      expect(singleCall.callType).to.equal(1)
+      expect(singleCall.logs).to.be.an("array").with.lengthOf(2)
+
+      const log1 = singleCall.logs[0]
+      expect(log1.$typeName).to.equal("sf.ethereum.type.v2.Log")
+      expect(log1.address).to.be.instanceOf(Buffer).with.lengthOf(20)
+      expect(log1.topics).to.be.an('array').with.lengthOf(4)
+      expect(log1.topics[0]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(log1.topics[1]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(log1.topics[2]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(log1.topics[3]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(log1.data).to.be.instanceOf(Buffer).with.lengthOf(160)
+      expect(log1.index).to.equal(0)
+      expect(log1.blockIndex).to.equal(4)
+      expect(log1.ordinal).to.be.a('bigint')
+
+      const log2 = singleCall.logs[1]
+      expect(log2.$typeName).to.equal("sf.ethereum.type.v2.Log")
+      expect(log2.address).to.be.instanceOf(Buffer).with.lengthOf(20)
+      expect(log2.topics).to.be.an('array').with.lengthOf(4)
+      expect(log2.topics[0]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(log2.topics[1]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(log2.topics[2]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(log2.topics[3]).to.be.instanceOf(Buffer).with.lengthOf(32)
+      expect(log2.data).to.be.instanceOf(Buffer).with.lengthOf(160)
+      expect(log2.index).to.equal(1)
+      expect(log2.blockIndex).to.equal(5)
+      expect(log2.ordinal).to.be.a('bigint')
+
+      expect(log2.ordinal).to.be.greaterThan(log1.ordinal)
     })
   })
 }
@@ -76,4 +149,3 @@ function isValidatorSetUpdateCall(call: Call): boolean {
   return isSameAddress(call.caller, systemAddress) &&
          isSameAddress(call.address, VALIDATOR_CONTRACT_ADDRESS)
 }
-
