bb5eec912cfe3a72c5de1e37980f7bee72f27041
Matthieu Vachon <matt@streamingfast.io>
2026-04-17 13:27:11 -0400

    Getting reth closer to Geth even in terms of ordinals

--- test/lib/constants.ts
+++ test/lib/constants.ts
@@ -2,34 +2,27 @@ export const besu_exclude_fields = [
   // Changes
   "calls[].balanceChanges",
   "calls[].gasChanges",
-  "calls[].nonceChanges[]",
-  "calls[].storageChanges[]",
-  "calls[].codeChanges[]",
+  "calls[].nonceChanges",
+  "calls[].storageChanges",
+  "calls[].codeChanges",
+
   // Ordinals
   "calls[].beginOrdinal",
   "calls[].endOrdinal",
   "calls[].logs[].ordinal",
   "endOrdinal",
   "receipt.logs[].ordinal",
+
   // Others
   "calls[].failureReason",
   "calls[].keccakPreimages",
 ]
 
 export const reth_exclude_fields = [
-  // Changes
-  // This is from v4 but we target v5 which don't save gas changes anymore, we will re-generate fully the
-  // snapshots at one point and remove excluded fields.
-  "calls[].gasChanges",
-
-  // Ordinals (we keep them for the very end when we will re-generate fully the snapshots and remove excluded fields)
+  // Ordinals (we have a bug to investigate with Deploys tests, order inversion on nonce <> code changes)
   "calls[].beginOrdinal",
   "calls[].endOrdinal",
-  "calls[].logs[].ordinal",
-  "calls[].balanceChanges[].ordinal",
-  "calls[].nonceChanges[].ordinal",
   "calls[].codeChanges[].ordinal",
+  "calls[].nonceChanges[].ordinal",
   "calls[].storageChanges[].ordinal",
-  "endOrdinal",
-  "receipt.logs[].ordinal",
 ]
